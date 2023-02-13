5dabcb57333dff611a5efe58897227cc57f50c6d
Katerina <katerina3113@bk.ru>
2023-02-13 23:47:36 +0300

    Домашнее задание 3

---
+++ hm2/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, 
+// которая будет находить строку с наименьшей суммой элементов.
+
+int[,] matrix = new int[5,5];
+Random rnd = new Random();
+InitMatrix(matrix);
+PrintMatrix(matrix);
+Console.WriteLine();
+TheRowOfMinSum(matrix);
+
+void InitMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1,10);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void TheRowOfMinSum(int[,] matrix)
+{
+    int MinInRow = 0;
+    int MinSum = 0;
+    int Sum = 0;
+    
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        MinInRow += matrix[0,i];
+    }
+    for (int i=0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) 
+        Sum += matrix[i,j];  
+    if (Sum < MinInRow)
+    {
+        MinInRow = Sum;
+        MinSum = i;
+    }
+    Sum = 0;
+    }
+    Console.Write($"Строка {MinSum + 1} содержит наименьшую сумму элементов");
+}
---
+++ hm3/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+Console.WriteLine("Введите данные для опредения матрицы: ");
+int a = InputNumbers("Введите колличество строк 1ой матрицы: ");
+int b = InputNumbers("Введите колличество строк 2ой матрицы: ");
+int c = InputNumbers("Введите число столбцов матриц: ");
+int diapason = InputNumbers("Введите максимальное значение заполнения матрицы; ");
+Random rnd = new Random();
+
+int[,] firstMartrix = new int[a, b];
+CreateArray(firstMartrix);
+Console.WriteLine($"Матрица №1:");
+WriteArray(firstMartrix);
+
+int[,] secomdMartrix = new int[b, c];
+CreateArray(secomdMartrix);
+Console.WriteLine($"Матрица №2:");
+WriteArray(secomdMartrix);
+
+int[,] resultMatrix = new int[a,c];
+
+MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
+Console.WriteLine($"Произведение матриц:");
+WriteArray(resultMatrix);
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = rnd.Next(diapason);
+    }
+  }
+}
+
+void WriteArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int z = 0; z < firstMartrix.GetLength(1); z++)
+      {
+        sum += firstMartrix[i,z] * secomdMartrix[z,j];
+      }
+      resultMatrix[i,j] = sum;
+    }
+  }
+}
